8e59476e8b663935fcef4ae9fd28d486ebf41813
maziyar <maziroadtodivine@gmail.com>
2024-02-14 16:54:37 +0330

    Work with file system and callbacks.

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-const os = require("os");
-const { add } = require("./math");
+const fs = require('fs')
+const path = require('path')
 
-console.log(os.type());
+fs.readFile(path.join(__dirname, 'files', 'text.txt'), 'utf-8', (err, data) => {
+    if (err) throw err
+    console.log(data);
+})
 
-console.log(add(1,2))
+fs.writeFile(path.join(__dirname, 'files', 'test.txt'), 'this is new', (err) => {
+    if (err) throw err
+    console.log('create file complete');
+    fs.appendFile(path.join(__dirname, 'files', 'test.txt'), '\n\n Yes tnq', err => {
+        if (err) throw err
+        console.log('modify complete');
+        fs.rename(path.join(__dirname, 'files', 'test.txt'), path.join(__dirname, 'files', 'tested.txt'), (e) => {
+            if (e) throw e
+            console.log('rename complete');
+        })
+    })
+})
+
+process.on('uncaughtException', err => {
+    console.log(`This is a err : ${err}`);
+    process.exit(1)
+})
