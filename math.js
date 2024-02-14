cd1e4a42d944dcfdabe145d79c103ea64315952a
maziyar <maziroadtodivine@gmail.com>
2024-02-14 14:29:29 +0330

    Change the way export some methods.

--- math.js
+++ math.js
@@ -1,7 +1,5 @@
-const add = (a, b) => a + b
-const sub = (a, b) => a - b
-const mul = (a, b) => a * b
-const div = (a, b) => a / b
+exports.add = (a, b) => a + b
+exports.sub = (a, b) => a - b
+exports.mul = (a, b) => a * b
+exports.div = (a, b) => a / b
 
-
-module.exports = { add, sub, mul, div }
